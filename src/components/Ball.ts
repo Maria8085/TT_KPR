e4db8059e8aa0aa1fab5de54cdb56f88d5b913d9
Maria8085 <52406953+Maria8085@users.noreply.github.com>
2024-03-22 12:21:05 +0400

    collision

---
+++ src/components/Ball.ts
@@ -0,0 +1,84 @@
+export type Color = string | CanvasGradient | CanvasPattern
+export type Vector = [number, number]
+
+export class Ball {
+    private friction: number = 0.99
+    private minSpeed: number = 0.005
+    private context: CanvasRenderingContext2D
+    public position: Vector = [0, 0]
+    public delta: Vector
+
+    public color: Color = "#00CC00"
+
+    public radius: number = 0;
+    public mass: number = 0;
+
+    constructor({context, position, radius, color, delta}: {
+        context: CanvasRenderingContext2D,
+        position: Vector,
+        radius: number,
+        color?: Color
+        delta?: Vector
+    }) {
+        this.context = context;
+        this.delta = delta ?? [Math.random() % 10, Math.random() % 10];
+        this.position = position;
+        this.radius = radius;
+        this.mass = (4 / 3) * Math.PI * Math.pow(radius, 3);
+        this.color = color ?? "#00CC00"
+    }
+
+    private checkWallCollision() {
+        const width = this.context.canvas.width
+        const height = this.context.canvas.height
+        if (this.position[0] + this.radius > width || this.position[0] - this.radius < 0) {
+            this.delta[0] = -this.delta[0];
+        }
+        if (this.position[1] + this.radius > height || this.position[1] - this.radius < 0) {
+            this.delta[1] = -this.delta[1]
+        }
+    }
+
+    updatePosition() {
+        this.position[0] += this.delta[0]
+        this.position[1] += this.delta[1]
+        this.checkWallCollision()
+
+        this.delta[0] *= this.friction
+        this.delta[1] *= this.friction
+
+        if(Math.abs(this.delta[0])<this.minSpeed){
+            this.delta[0] = 0
+        }
+        if(Math.abs(this.delta[1])<this.minSpeed){
+            this.delta[1] = 0
+        }
+    }
+
+
+
+    public checkBallCollision(ball: Ball) {
+        const distance = Math.sqrt(
+            (this.position[0] - ball.position[0]) **  2 + (this.position[1] - ball.position[1]) ** 2
+    );
+        if (distance < this.radius + ball.radius) {
+            this.delta[0] = -this.delta[0];
+            this.delta[1] = -this.delta[1];
+            ball.delta[0] = -ball.delta[0];
+            ball.delta[1] = -ball.delta[1];
+        }
+    }
+
+    draw() {
+        const currentColor = this.context.fillStyle
+        this.context.beginPath();
+        this.context.fillStyle = this.color;
+        this.context.strokeStyle = '#003300';
+        this.context.arc(this.position[0], this.position[1], this.radius, 0, 2 * Math.PI, false);
+        this.context.fill();
+        this.context.lineWidth = 5;
+        this.context.stroke();
+        this.context.closePath();
+        this.context.fillStyle = currentColor;
+    }
+}
--- src/toDo.ts
+++ src/toDo.ts
@@ -1,23 +1,36 @@
-import { Circle } from "./components/Circle";
+import {Ball} from "./components/Ball.ts";
 
 
-
-export function initialCanvas(canvas: HTMLCanvasElement){
+export function initialCanvas(canvas: HTMLCanvasElement) {
     const context = canvas.getContext('2d');
+    if (!context) {
+        return null
+    }
     const shapes = [
-        new Circle(100,100,50, "#ff00ff"),
-        new Circle(350,200,100),
-    ] 
-    function draw(){
-        if(!context){
+        new Ball({
+            context, position: [100, 100], radius: 50, color: "#ff00ff", delta: [2, 0]
+        }),
+        new Ball({
+            context, position: [300, 100], radius: 20, color: "#00ff00", delta: [-2, 0]
+        }),
+    ]
+
+    function draw() {
+        if (!context) {
             return null;
         }
         context.beginPath();
         context.fillStyle = '#0c5705';
-        context.fillRect(0, 0, canvas.width, canvas.height); 
+        context.fillRect(0, 0, canvas.width, canvas.height);
         context.closePath();
-        shapes.forEach(shape=>{
-            shape.draw(context);
+        shapes.forEach((shape,index) => {
+            shape.updatePosition();
+            shapes.forEach((otherBall, otherIndex) => {
+                if(index!==otherIndex){
+                    shape.checkBallCollision(otherBall)
+                }
+            })
+            shape.draw();
         });
         window.requestAnimationFrame(draw);
     }
